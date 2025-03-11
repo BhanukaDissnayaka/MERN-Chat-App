00db7b8976c2c21e7cfc10ad6151117278ee7b40
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-03-11 15:34:53 +0530

    SignUp Ui Added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div data-theme="dark">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />}></Route>
       </Routes>
     </div>
   );

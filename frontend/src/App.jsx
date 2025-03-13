0fc38fb21061bcd54a6dc888d6c42c7e8792321c
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-03-13 19:29:30 +0530

    Profile page ui added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import SigninPage from "./pages/SignInPage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />}></Route>
         <Route path="/signin" element={<SigninPage />}></Route>
+        <Route path="/profile" element={<ProfilePage />}></Route>
       </Routes>
     </div>
   );

165baf15a0a3113b483858abc5142e2a7cd08230
Bhanuka Dissanayaka <bhanukadissanayaka2005@gmail.com>
2025-04-01 22:16:31 +0530

    fix(server): enhance API configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,21 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRouter from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
 
+app.use(express.json({ limit: "10mb" })); // Adjust the limit as needed
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
